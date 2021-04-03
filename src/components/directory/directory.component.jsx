28672d6645fc7bbb11e1fecfe345172c5440f30a
jasonxgilchrist <jasonxgilchrist@gmail.com>
2021-04-03 13:50:33 +0530

    updated data.json file

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -17,30 +17,35 @@ export default class Directory extends Component {
         {
           title: "BOOKS",
           imageUrl: 'https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg',
+          // imageUrl: require("../../assets/books.jpeg"),
           id: 1,
           linkUrl: 'books'
         },
         {
           title: "ELECTRONICS",
           imageUrl: 'https://i0.wp.com/movingtips.wpengine.com/wp-content/uploads/2017/01/appliances.jpg?fit=600%2C360&ssl=1',
+          // imageUrl: require("../../assets/electronics.jpg"),
           id: 2,
           linkUrl: 'electronics'
         },
         {
           title: "FURNITURE",
           imageUrl: 'https://www.betterhomeindia.com/wp-content/uploads/2019/03/flora-bedroomset-betterhomeindia.jpg',
+          // imageUrl: require("../../assets/furniture.jpg"),
           id: 3,
           linkUrl: 'furniture'
         },
         {
           title: "WOMENS",
           imageUrl: "https://styleyesfashioncrate.com/wp-content/uploads/2019/07/Styleyes-Fashion-Crate-10-Womens-Fashion-Trends-Making-A-Comeback-In-2019.jpg",
+          // imageUrl: require("../../assets/womensfashion.jpg"),
           id: 4,
           linkUrl: 'womens'
         },
         {
           title: "MENS",
           imageUrl: 'https://images.indianexpress.com/2018/01/men-fashion-759-filephoto.jpg',
+          // imageUrl: require("../../assets/mensfashion.jpg"),
           id: 5,
           linkUrl:'mens'
         },
--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,154 +1,154 @@
 const  ShopData = [
     {
         id: 1,
-        title: 'Hats',
-        routeName: 'hats',
+        title: 'Books',
+        routeName: 'books',
         items: [
           {
             id: 1,
-            name: 'Brown Brim',
-            imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-            price: 25
+            name: 'Life is What You Make it',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kj0bp8w0-0/book/h/v/s/life-is-what-you-make-it-original-imafynyk8q28wwgq.jpeg?q=70',
+            price: 150
           },
           {
             id: 2,
-            name: 'Blue Beanie',
-            imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-            price: 18
+            name: 'Think And Grow Rich',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kf5pzm80/book/8/8/4/think-and-grow-rich-original-imafvztnm7kzdzz9.jpeg?q=70',
+            price: 225
           },
           {
             id: 3,
-            name: 'Brown Cowboy',
-            imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-            price: 35
+            name: 'Messed Up! But All For Love',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/ki96c280-0/book/m/k/i/messed-up-but-all-for-love-original-imafy3a9smgz8crv.jpeg?q=70',
+            price: 175
           },
           {
             id: 4,
-            name: 'Grey Brim',
-            imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-            price: 25
+            name: 'You Are The Best Wife',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/jyeq64w0/book/5/4/0/you-are-the-best-wife-original-imafgkyugczdayt8.jpeg?q=70',
+            price: 175
           },
           {
             id: 5,
-            name: 'Green Beanie',
-            imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-            price: 18
+            name: 'Her Last Wish',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/jplif0w0/book/8/7/8/her-last-wish-original-imafbt2huwgfzzj8.jpeg?q=70',
+            price: 199
           },
           {
             id: 6,
-            name: 'Palm Tree Cap',
-            imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-            price: 14
+            name: 'When Love Came Calling',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/k7m8brk0/book/9/1/1/when-love-came-calling-original-imafpt7fu6khz9ks.jpeg?q=70',
+            price: 199
           },
           {
             id: 7,
-            name: 'Red Beanie',
-            imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-            price: 18
+            name: 'The Girl Next Door',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kekadu80/book/4/6/1/the-girl-next-door-original-imafv7w52zhjabsp.jpeg?q=70',
+            price: 199
           },
           {
             id: 8,
-            name: 'Wolf Cap',
-            imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-            price: 14
+            name: 'One Arranged Murder',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kdxfc7k0/book/1/3/9/one-arranged-murder-original-imafuqfpfthrvzsz.jpeg?q=70',
+            price: 225
           },
           {
             id: 9,
-            name: 'Blue Snapback',
-            imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-            price: 16
+            name: 'The Psychology Of Money',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kjom6q80-0/regionalbooks/o/i/t/the-psychology-of-money-original-imafvb5vbgcczykj.jpeg?q=70',
+            price: 399
           }
         ]
       },
       {
         id: 2,
-        title: 'Sneakers',
-        routeName: 'sneakers',
+        title: 'Electronics',
+        routeName: 'electronics',
         items: [
           {
             id: 10,
-            name: 'Adidas NMD',
-            imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-            price: 220
+            name: 'MI 4A PRO TV',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kc54ivk0/television/r/q/g/mi-l43m4-4ain-original-imaftc5fy6bfmhyb.jpeg?q=70',
+            price: 14999
           },
           {
             id: 11,
-            name: 'Adidas Yeezy',
-            imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-            price: 280
+            name: 'Samsung 80cm TV',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kbs9k7k0/television/c/j/3/samsung-ua32t4340akxxl-original-imaft25qdysfsq7k.jpeg?q=70',
+            price: 16490
           },
           {
             id: 12,
-            name: 'Black Converse',
-            imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-            price: 110
+            name: 'LG 80m TV',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kdyus280/television/g/d/q/lg-32lm565bpta-32lm565bpta-original-imafurgkdrjfggbw.jpeg?q=70',
+            price: 16499
           },
           {
             id: 13,
-            name: 'Nike White AirForce',
-            imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-            price: 160
+            name: 'Oneplus Y Series TV',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kfzq8i80/television/e/j/y/oneplus-32h0a00-original-imafwb6wmqxh2ung.jpeg?q=70',
+            price: 19990
           },
           {
             id: 14,
-            name: 'Nike Red High Tops',
-            imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-            price: 160
+            name: 'Iphone XR Red',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/jnj7iq80/mobile/3/f/a/apple-iphone-xr-mry62hn-a-original-imafa6zkhyceh8dg.jpeg?q=70',
+            price: 52900
           },
           {
             id: 15,
-            name: 'Nike Brown High Tops',
-            imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-            price: 160
+            name: 'Samsung S20+',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/k6mibgw0/mobile/8/v/h/samsung-galaxy-s20-sm-g985fzadinu-original-imafpfkbqg3hpmgt.jpeg?q=70',
+            price: 64999
           },
           {
             id: 16,
-            name: 'Air Jordan Limited',
-            imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-            price: 190
+            name: 'Samsung S10 Lite',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/k5o7r0w0/mobile/h/s/v/samsung-galaxy-s10-lite-sm-g770fzksinu-original-imafzaqnbjuffrvv.jpeg?q=70',
+            price: 51999
           },
           {
             id: 17,
-            name: 'Timberlands',
-            imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-            price: 200
+            name: 'Google Pixel 4a',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kflftzk0/mobile/h/q/u/google-pixel-4a-ga02099-in-original-imafwyzmnfxzmv5q.jpeg?q=70',
+            price: 31999
           }
         ]
       },
       {
         id: 3,
-        title: 'Jackets',
-        routeName: 'jackets',
+        title: 'Furniture',
+        routeName: 'furniture',
         items: [
           {
             id: 18,
-            name: 'Black Jean Shearling',
-            imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-            price: 125
+            name:'Furniture 1',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/kfmv9u80/bed/a/h/6/king-na-particle-board-bkwen00hhbs101p-flipkart-perfect-homes-original-imafwft4heyc6syx.jpeg?q=70',
+            price: 20999
           },
           {
             id: 19,
-            name: 'Blue Jean Jacket',
-            imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-            price: 90
+            name: 'Furniture 2',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/k5wse4w0/bed/s/b/v/queen-na-particle-board-bqwen00nhbs200p-flipkart-perfect-homes-original-imaf9fz37aubvwk3.jpeg?q=70',
+            price: 13999
           },
           {
             id: 20,
-            name: 'Grey Jean Jacket',
-            imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-            price: 90
+            name: 'Furniture 3',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/k5ihzm80/bed/9/n/n/queen-na-particle-board-rome-exotic-teak-bharat-lifestyle-exotic-original-imafhz5yaapzwup4.jpeg?q=70',
+            price: 49999
           },
           {
             id: 21,
-            name: 'Brown Shearling',
-            imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-            price: 165
+            name: 'Furniture 4',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/k5ihzm80/bed/z/u/a/king-na-particle-board-fo-jaskbox-01-forzza-wenge-original-imafz6eusufcdqgk.jpeg?q=70',
+            price: 33900
           },
           {
             id: 22,
-            name: 'Tan Trench',
-            imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-            price: 185
+            name: 'Furnitiure 5',
+            imageUrl: 'https://rukminim1.flixcart.com/image/416/416/k4n2avk0/bed/p/z/g/king-na-rosewood-sheesham-wswb7872samd-wakefit-natural-original-imafn7gkupvzqvzy.jpeg?q=70',
+            price: 22000
           }
         ]
       },
@@ -161,31 +161,31 @@ const  ShopData = [
             id: 23,
             name: 'Blue Tanktop',
             imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-            price: 25
+            price: 250
           },
           {
             id: 24,
             name: 'Floral Blouse',
             imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-            price: 20
+            price: 200
           },
           {
             id: 25,
             name: 'Floral Dress',
             imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-            price: 80
+            price: 800
           },
           {
             id: 26,
             name: 'Red Dots Dress',
             imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-            price: 80
+            price: 800
           },
           {
             id: 27,
             name: 'Striped Sweater',
             imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-            price: 45
+            price: 450
           },
           {
             id: 28,
@@ -197,7 +197,7 @@ const  ShopData = [
             id: 29,
             name: 'White Blouse',
             imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-            price: 20
+            price: 200
           }
         ]
       },
@@ -216,31 +216,31 @@ const  ShopData = [
             id: 31,
             name: 'Floral T-shirt',
             imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-            price: 20
+            price: 200
           },
           {
             id: 32,
             name: 'Black & White Longsleeve',
             imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-            price: 25
+            price: 250
           },
           {
             id: 33,
             name: 'Pink T-shirt',
             imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-            price: 25
+            price: 250
           },
           {
             id: 34,
             name: 'Jean Long Sleeve',
             imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-            price: 40
+            price: 400
           },
           {
             id: 35,
             name: 'Burgundy T-shirt',
             imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-            price: 25
+            price: 250
           }
         ]
       }
